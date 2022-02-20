17a53371672975721f9de42b53ef3145be08ab35
SoumyaeCodes <89376978+SoumyaeCodes@users.noreply.github.com>
2022-02-20 18:11:14 +0530

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+define('EMAIL','idiscuss.services@gmail.com');
+define('PASSWORD','idiscuss3002');
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','discnet');
+?>
